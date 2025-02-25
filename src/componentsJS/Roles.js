6f675a423e3837a58c18e61c55e9f68b3734aa26
Avital-Ha <avital.hamburg@gmail.com>
2025-02-25 22:41:23 +0200

    סיום הוספה של כל הדאטה

--- src/componentsJS/Roles.js
+++ src/componentsJS/Roles.js
@@ -1,53 +1,32 @@
 import React from 'react';
 import '../componentsCss/Roles.css';
 
-const Roles = ({ selectedScenario, onRoleSelect, onEvacuationClick }) => {
+const Roles = ({ selectedScenario, onRoleSelect }) => {
   return (
     <div id="roles-container">
       {/* Render the selected scenario information */}
-      {selectedScenario.situation !== "פינוי ישוב" && (
-        <div className="scen-selected">
-          <img
-            className='selected-icon'
-            src={`${process.env.PUBLIC_URL}/assets/iconsGrey/icon${selectedScenario.id}.svg`}
-            alt={`${selectedScenario.situation} icon`} 
-          />
-          <p className='selected-name'>{selectedScenario.situation}</p>
-        </div>
-      )}
+      <div className="scen-selected">
+        <img
+          className='selected-icon'
+          src={`${process.env.PUBLIC_URL}/assets/iconsGrey/icon${selectedScenario.id}.svg`}
+          alt={`${selectedScenario.situation} icon`} 
+        />
+        <p className='selected-name'>{selectedScenario.situation}</p>
+      </div>
 
-      {/* Check if the selected scenario is "פינוי ישוב" */}
-      {selectedScenario.situation === "פינוי ישוב" ? (
-        <div id="evacuation">
-          <img
-            className='scenrio-icon'
-            src={`${process.env.PUBLIC_URL}/assets/iconsGrey/icon${selectedScenario.id}.svg`}
-            alt={`${selectedScenario.situation} icon`} 
-          />
-          <p className='scenrio-name'>{selectedScenario.situation}</p>
-          <p className='evacuation-dis'>
-            במצב זה אין בעלי תפקידים עם משימות מוסדרות מפני שמפנים את הישוב.
-            לחצו על הכפתור למעבר.
-          </p>
-          <button className='next-btnev' onClick={onEvacuationClick}>
-            מעבר למצב פינוי ישוב
+      <div id="buttons-container">
+        {/* Render only the first seven role buttons */}
+        {selectedScenario.roles.slice(0, 7).map((role, index) => (
+          <button
+            className='role-button'
+            key={index}
+            style={{ backgroundColor: role.color }}
+            onClick={() => onRoleSelect(role)} // Call function on role button click
+          >
+            {role.role} {/* Role name */}
           </button>
-        </div>
-      ) : (
-        <div id="buttons-container">
-          {/* Render role buttons for other scenarios */}
-          {selectedScenario.roles.map((role, index) => (
-            <button
-              className='role-button'
-              key={index}
-              style={{ backgroundColor: role.color }}
-              onClick={() => onRoleSelect(role)} // Call function on role button click
-            >
-              {role.role} {/* Role name */}
-            </button>
-          ))}
-        </div>
-      )}
+        ))}
+      </div>
     </div>
   );
 };
