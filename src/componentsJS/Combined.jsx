beaaaaf75e2d9b0d2b7438e41c3700947ce03f08
AvitalHamburg <choenmatilda@gmail.com>
2025-04-06 11:19:14 +0300

    Change to jsx

--- src/componentsJS/Combined.jsx
+++ src/componentsJS/Combined.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Messages from '../componentsJS/Messages.js';
+import Messages from './Messages.js';
 import '../componentsCss/Combined.css';
 
 const Combined = ({ selectedScenario, selectedRole }) => {
--- src/componentsJS/Header.jsx
+++ src/componentsJS/Header.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../componentsCss/Header.css';
-import Hamburger from '../componentsJS/Hamburger'; 
-import NavBar from '../componentsJS/NavBar'; 
+import Hamburger from './Hamburger'; 
+import NavBar from './NavBar'; 
 
 function Header() {
   const navigate = useNavigate();
