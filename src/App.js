92c46ff11f9083f385ba06d33a6c77978c414a65
AvitalHamburg <choenmatilda@gmail.com>
2025-06-25 12:14:41 +0300

    קלקול

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
-// import Home from './componentsJS/Home';
-// import Header from './componentsJS/Header';
-// import Intro from './componentsJS/Intro';
-// import Credits from './componentsJS/Credits';
-// import EmergencyRolls from './componentsJS/EmergencyRolls';
-// import PdfFiles from './componentsJS/PdfFiles';
+import Home from './componentsJS/Home';
+import Header from './componentsJS/Header';
+import Intro from './componentsJS/Intro';
+import Credits from './componentsJS/Credits';
+import EmergencyRolls from './componentsJS/EmergencyRolls';
+import PdfFiles from './componentsJS/PdfFiles';
 
 function App() {
     return (
         <div className="App">
-            <Header className="header-fixed" />
+            <div style={{ textAlign: 'center', marginTop: '20%' }}>
+            הלומדה בתיקון
+        </div>
+            {/* <Header className="header-fixed" />
             <Routes>
                 <Route path="/" element={<Navigate to="/tzahi" />} />
                 <Route path="/tzahi" element={<Intro />} />
@@ -20,7 +23,7 @@ function App() {
                 <Route path="/tzahi/Credits" element={<Credits />} />
                 <Route path="/tzahi/PdfFiles" element={<PdfFiles />} />
                 <Route path="/tzahi/EmergencyRolls/:scenarioId" element={<EmergencyRolls />} />
-            </Routes>
+            </Routes> */}
         </div>
     );
 }
